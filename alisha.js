00e33493e96d0f3ccae2cf6591755d1db92fb047
alisha zehra <alishazehra866@gmail.com>
2024-09-06 10:58:25 +0500

    first commit

---
+++ alisha.js
@@ -0,0 +1,12 @@
+var _a;
+(_a = document.getElementById('resumeForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('skills').value;
+    var resumeOutput = "\n        <h2>Resume</h2>\n        <p><strong>Name:</strong> ".concat(name, "</p>\n        <p><strong>Email:</strong> ").concat(email, "</p>\n        <p><strong>Phone:</strong> ").concat(phone, "</p>\n        <h3>Education</h3>\n        <p>").concat(education, "</p>\n        <h3>Work Experience</h3>\n        <p>").concat(experience, "</p>\n        <h3>Skills</h3>\n<p>").concat(skills, "</p>\n    ");
+    document.getElementById('resumeOutput').innerHTML = resumeOutput;
+});
---
+++ alisha.ts
@@ -0,0 +1,26 @@
+document.getElementById('resumeForm')?.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = (document.getElementById('name') as HTMLInputElement).value;
+    const email = (document.getElementById('email') as HTMLInputElement).value;
+    const phone = (document.getElementById('phone') as HTMLInputElement).value;
+    const education = (document.getElementById('education') as HTMLTextAreaElement).value;
+    const experience = (document.getElementById('experience') as HTMLTextAreaElement).value;
+    const skills = (document.getElementById('skills') as HTMLTextAreaElement).value;
+
+    const resumeOutput = `
+        <h2>Resume</h2>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Phone:</strong> ${phone}</p>
+        <h3>Education</h3>
+        <p>${education}</p>
+        <h3>Work Experience</h3>
+        <p>${experience}</p>
+        <h3>Skills</h3>
+<p>${skills}</p>
+    `;
+
+    document.getElementById('resumeOutput')!.innerHTML = resumeOutput;
+});
+
